4713e82212e365ca3000797d45efeacbfa6ea52f
johndoe-dev-ai <mshrivastava6007@gmail.com>
2024-09-12 16:26:47 +0530

    Update glue_job.py

--- glue_job.py
+++ glue_job.py
@@ -1,51 +1,30 @@
 import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
+from pyspark.sql import SparkSession
 
-def convert_csv_to_parquet(glueContext, raw_bucket, raw_key, prep_bucket, prep_key):
-    # Create dynamic frame from CSV
-    dynamic_frame = glueContext.create_dynamic_frame.from_options(
-        format_options={"quoteChar": '"', "withHeader": True, "separator": ","},
-        connection_type="s3",
-        format="csv",
-        connection_options={
-            "paths": [f"s3://{raw_bucket}/{raw_key}"],
-            "recurse": True,
-        },
-        transformation_ctx="datasource0",
-    )
+def convert_csv_to_parquet(spark, input_path, output_path):
+    # Read CSV
+    df = spark.read.option("header", "true").csv(input_path)
     
-    # Convert to Parquet and write to prep bucket
-    glueContext.write_dynamic_frame.from_options(
-        frame=dynamic_frame,
-        connection_type="s3",
-        format="parquet",
-        connection_options={
-            "path": f"s3://{prep_bucket}/{prep_key}",
-            "partitionKeys": [],
-        },
-        transformation_ctx="datasink1",
-    )
+    # Write Parquet
+    df.write.parquet(output_path, mode="overwrite")
 
-def run_job(raw_bucket, raw_key, prep_bucket, prep_key):
-    args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-    sc = SparkContext()
-    glueContext = GlueContext(sc)
-    spark = glueContext.spark_session
-    job = Job(glueContext)
-    job.init(args['JOB_NAME'], args)
-    
-    convert_csv_to_parquet(glueContext, raw_bucket, raw_key, prep_bucket, prep_key)
-    
-    job.commit()
+def run_glue_job(glue_context, params):
+    spark = glue_context.spark_session
+    convert_csv_to_parquet(spark, params['input_path'], params['output_path'])
 
 if __name__ == "__main__":
-    run_job(
-        raw_bucket=sys.argv[1],
-        raw_key=sys.argv[2],
-        prep_bucket=sys.argv[3],
-        prep_key=sys.argv[4]
-    )
+    from awsglue.context import GlueContext
+    from awsglue.job import Job
+    from awsglue.utils import getResolvedOptions
+
+    args = getResolvedOptions(sys.argv, ['JOB_NAME', 'input_path', 'output_path'])
+    glue_context = GlueContext(SparkContext.getOrCreate())
+    job = Job(glue_context)
+    job.init(args['JOB_NAME'], args)
+
+    run_glue_job(glue_context, {
+        'input_path': args['input_path'],
+        'output_path': args['output_path']
+    })
+
+    job.commit()
